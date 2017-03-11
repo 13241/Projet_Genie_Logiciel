2cf7d357522f93c9dd7605e890961af1018ea3fe
0Hub0 <hubertmgb@gmail.com>
2017-03-11 14:49:59 +0100

    Add files via upload

---
+++ Kitbox/Part.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitbox
+{
+    abstract class Part
+    {
+    }
+    class Cleat:Part
+    {
+
+    }
+}
