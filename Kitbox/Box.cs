024a6c8e85381cf41f0a5dec1e92d897c8df810c
0Hub0 <hubertmgb@gmail.com>
2017-03-18 21:08:35 +0100

    Update Box.cs
    Must add the implementation that counts the number of piece in the dico parts

--- Kitbox/Box.cs
+++ Kitbox/Box.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,24 +23,49 @@ namespace Kitbox
         }
         
 
-
         public bool IsComplete()
         {
+            
             bool res = false;
 
-            // Compare le nombre de fois qu'on a les éléments constitutifs d'une box; SI on le bon nombre, res =true et on renvoie res
-            if (tasseaux == 4 & traverseAV == 2 & travaerseAR == 2 & traverseGD == 4 & panneauxHB == 2 & panneauxGD == 2 & panneauAR == 1 & door == true)
+
+            // Initilise objet de la classe Door
+            Door test = new Door();
+            Cleat cleat = new Cleat();
+
+            // Compare le nombre de fois qu'on a les éléments constitutifs d'une box; Si on a le bon nombre, res =true et on renvoie res
+            if (tasseaux == 4 & traverseAV == 2 & travaerseAR == 2 & traverseGD == 4 & panneauxHB == 2 & panneauxGD == 2 & panneauAR == 1 & test.tDoor == true || false & cleat == 4 parts.TryGetValue())
             { res = true; }
 
             return res;
+            /*
+             Idées pour compter le # de fois que pièce est dans parts
+             
+            A:
+                - Faire liste pour chaque pièce, liste contenant les références de chaque pièce prisent par sql
+                - Puis comparer clé du dico parts (reference) avec la liste référence de la piste corrrespondante
+            
+            B:
+                - Créer une instance de chaque pièce; 
+                - Mettre un compteur dans chaque instance, compteur comtabilisant le nbre de fois que l'on a créé une instance à partir de la classe.
+
+             */
         }
 
-        // Setter existe déjà => Utiliter de SetHeight??
+        
         // hauteur casier = hauter tasseaux+2x2cm (hauteur traverses)
         public void SetHeight(double height)
         {
             this.height = height;
-          
+
+            // Ajouter cleats 
+            // Valeur initiale de i est identique à la position => Elle est modifiable
+            for (int i =0;i<4;i++)
+            {
+                Cleat cleat = new Cleat();
+                string refer = " ";               // Variable contenant la référence de la cornière (Via sql?)
+                Add(cleat,i.ToString(),refer);
+            }          
         }
 
         // ajouter paramètre reference? - Que faire si part n'a pas de position
@@ -60,12 +85,12 @@ namespace Kitbox
                 
              */
 
-            new_Part.Clear();
+
+
+            new_Part.Clear();   // Vide le dico pour être sûr qu'il n'y pas d'élément superflu ajouté
             new_Part.Add(position, newP);  // Add new part in the Dictionary new_Parts [Create part]
              
-            parts.Add(reference,new_Part);    // Add new part in the Dictionry parts [Add part in dico]
-            
-            
+            parts.Add(reference,new_Part);    // Add new part in the Dictionry parts [Add part in dico]   
         }
 
         // N'as pas besoin du paramètre position car chaque part a une ref unique
@@ -79,7 +104,7 @@ namespace Kitbox
         
         public Dictionary<string,Dictionary<string,Part>> GetParts()
         {
-            return Parts; // Send the Dictionary Parts
+            return parts; // Send the Dictionary Parts
         }
 
     }
