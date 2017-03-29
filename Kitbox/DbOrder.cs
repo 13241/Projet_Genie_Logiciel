16ae3334013328104fe8c97780cdc7e3e03174d6
13241 <13241@ecam.be>
2017-03-29 13:16:17 +0200

    Corrected DbOrder

--- Kitbox/DbOrder.cs
+++ Kitbox/DbOrder.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -9,7 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
->>>>>>> ac6f694f81b607de6717b43523c2fbff11154698
 namespace Kitbox
 {
 	public class DbOrder
@@ -19,9 +16,6 @@ namespace Kitbox
 		}
 		public void DbConnectOrder(string[] parameters)
 		{
-<<<<<<< HEAD
-			
-=======
 			// https://www.codeproject.com/Articles/823854/How-to-connect-SQL-Database-to-your-Csharp-program
 			SqlConnection conn = new SqlConnection();
 			conn.ConnectionString = "connection_string";
@@ -46,7 +40,6 @@ namespace Kitbox
 					reader[0], reader[1], reader[2], reader[3]));
 				}
 			}
->>>>>>> ac6f694f81b607de6717b43523c2fbff11154698
 		}
 		public void DbAddOrder(Order order)
 		{
