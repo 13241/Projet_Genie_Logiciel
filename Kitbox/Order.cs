156d6a6ad42ef3776dc240856dd62f8d5fec92d9
13241 <13241@ecam.be>
2017-06-16 23:40:09 +0200

    Order.cs

--- Kitbox/Order.cs
+++ Kitbox/Order.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 namespace Kitbox
 {
 	public class Order
 	{
 		protected Person current_client;
-		protected List<Wardrobe> wardrobes;
-		protected Dictionary<string, object> bill;
-		protected List<string> parts_list;
-		protected Dictionary<string, string> codes;
+		protected List<object> wardrobes = new List<object>();
+        protected Dictionary<string, object> bill = new Dictionary<string, object>();
+        protected List<string> parts_list = new List<string>();
+        protected Dictionary<string, string> codes = new Dictionary<string, string>();
 		public Order()
 		{
 		}
@@ -38,21 +39,23 @@ namespace Kitbox
 		{
 
 		}
-		public Dictionary<string, object> GetBill(int order_id)
-		{
-			
+        public Dictionary<string, object> GetBill(int order_id)
+        {
+            //Dictionary<string, Dictionary<string, List<Dictionary<string, List<object>>>>> bill = new Dictionary<string, Dictionary<string, List<Dictionary<string, List<object>>>>>();
+            List<List<object>> list = new List<List<object>>();
 
-			List<List<object>> list = new List<List<object>>();
-			string RelTableName = "rel_Cat_Ord";
-			string OrderTableName = "orders";
-			string columnNames;
-			string condition = string.Format("WHERE (Order_Id = {0})", order_id);
-			double total_price = 0;
+            string RelTableName = "rel_catord";
+            string OrderTableName = "orders";
+            string columnNames;
+            string condition = string.Format("WHERE (Order_Id = {0})", order_id);
+            double total_price = 0;
 
-			bill["Header"] = DbOrder.DbSearchOrder(OrderTableName, "Header_Bill", string.Format("WHERE (Order_Id = {0} AND Client_Id = {1})", order_id, current_client));
-
+             
+            list = DbOrder.DbSearchOrder(OrderTableName, "Header_Bill", string.Format("WHERE (Order_Id ='{0}' AND Client_Id ='{1}')", order_id, current_client.Id));
+            bill.Add("Header",list[0][0]);
 			columnNames = "DISTINCT Wardrobe_Id";
 			list = DbOrder.DbSearchOrder(RelTableName, columnNames, condition);
+            bill.Add("Components","");
             for (int i = 1; i <= list.Count; i++)
             {
                 bill["Components"] += string.Format("Wardrobe {0} :\n\t", i);
@@ -65,6 +68,7 @@ namespace Kitbox
                     bill["Components"] += string.Format("Box {0} :\n\t\t", i);
                     columnNames = "Component_Id";
                     condition = string.Format("WHERE (Order_Id = {0} AND Wardrobe_Id = {1} AND Box_Id = {2})", order_id, i, j);
+                    //Pas celui la
                     list = DbOrder.DbSearchOrder(RelTableName, columnNames, condition);
 
                     for (int k = 1; k <= list.Count; k++)
@@ -96,30 +100,18 @@ namespace Kitbox
 					}
 				}
 			}
-
-            bill["Header"] += String.Format("\n\nPrix total : {0} €", Convert.ToString(total_price));
-
-			bill["Footer"] = DbOrder.DbSearchOrder(OrderTableName, "Footer_Bill", string.Format("WHERE (Order_Id = {0} AND Client_Id = {1})", order_id, current_client));
+            //Ici
+			bill["Footer"] = DbOrder.DbSearchOrder(OrderTableName, "Footer_Bill", string.Format("WHERE (Order_Id = {0} AND Client_Id = {1})", order_id, current_client.Id));
 
 			return bill;
 		}
-		public List<string> GetPartsList()
-		{
-			// on renvoie juste la liste ???
-			return parts_list;
-		}
-		public Dictionary<string, string> GetCodes()
-		{
-			// on renvoie juste le dico???
-			return codes;
-		}
-		public Person GetCurrentClient
-		{
-			get { return this.current_client;}
-		}
-		public List<Wardrobe> GetWardrobes
-		{
-			get { return this.wardrobes;}
-		}
+		public List<string> PartsList{ get => parts_list; set => parts_list = value; }
+		
+		public Dictionary<string, string> Codes{ get => codes; set => codes = value; }
+		
+		public Person CurrentClient { get => current_client; set => current_client = value; }
+
+		public List<object> Wardrobes{ get => wardrobes; set => wardrobes = value; }
+		
 	}
 }
