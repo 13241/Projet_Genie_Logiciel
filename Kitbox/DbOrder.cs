e4880848d542f2e25b7c4751034e2ffb61d62a91
13241 <13241@ecam.be>
2017-06-16 10:43:06 +0200

    _

--- Kitbox/DbOrder.cs
+++ Kitbox/DbOrder.cs
@@ -1,60 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
+using System.Globalization;
 
->>>>>>> ac6f694f81b607de6717b43523c2fbff11154698
+
 namespace Kitbox
 {
-	public class DbOrder
-	{
-		public DbOrder()
+    static class DbOrder
+    {
+        static void DbAddOrder(Order order)
+        {
+            string columnNames = "Order_Id, Client_Id, Date, Header_Bill, Footer_Bill";
+            string tableName = "order_informations";
+            string Client_Id = order.GetCurrentClient.GetId.ToString();
+            BDD database = new BDD("kitbox");
+            DateTime date = DateTime.Now;
+            string dateString = date.ToString();
+            string data = "1," + Client_Id + ","+dateString+",Header_Bill,Footer_Bill";
+            database.addElement(tableName, columnNames, data);
+        }
+        static Dictionary<string, object> DbSearchClient(int id_client)
+        {
+            Dictionary<string, object> order = new Dictionary<string, object>();
+
+            /////////////Use of the Bill dictionary
+
+            Dictionary<string, object> bill;
+            bill = textString.bill();
+
+            ////////////Components part list
+
+            //List<string> Parts_list = new List<string>();
+
+            return order;
+        }
+
+		public static List<List<object>> DbSearchOrder(string tableName, string columnNames, string condition)
 		{
+			List<List<object>> result = new List<object>();
+
+			//string columnNames = "DISTINCT Component_Id, Wardrobe_Id, Box_ID";
+			//string tableName = "rel_Cat_Ord";
+			//string condition = string.Format("WHERE (Order_Id = {0})", order_id);
+			BDD database = new BDD("kitbox");
+
+			result = database.readelement(columnNames, tableName, condition);
+
+			return result;
 		}
-		public void DbConnectOrder(string[] parameters)
-		{
-<<<<<<< HEAD
-			
-=======
-			// https://www.codeproject.com/Articles/823854/How-to-connect-SQL-Database-to-your-Csharp-program
-			SqlConnection conn = new SqlConnection();
-			conn.ConnectionString = "connection_string";
-			conn.Open();
-
-			// use the connection here
-
-			conn.Close();
-			conn.Dipose();
-			SqlCommand command = new SqlCommand("SELECT * FROM TableName", conn);
-			SqlCommand command = new SqlCommand("SELECT * FROM TableName WHERE FirstColumn = @0", conn);
-
-			// Create new SqlDataReader object and read data from the command.
-			using (SqlDataReader reader = command.ExecuteReader())
-			{
-				// while there is another record present
-				while (reader.Read())
-				{
-					// write the data on to the screen
-					Console.WriteLine(String.Format("{0} \t | {1} \t | {2} \t | {3}",
-					// call the objects from their index
-					reader[0], reader[1], reader[2], reader[3]));
-				}
-			}
->>>>>>> ac6f694f81b607de6717b43523c2fbff11154698
-		}
-		public void DbAddOrder(Order order)
-		{
-
-		}
-		public Dictionary<string, object> DbSearchClient(int id_client)
-		{
-
-		}
-	}
+    }
 }
