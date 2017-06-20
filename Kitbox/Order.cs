6cbe7f2c908de19322823ea8b199da6b10f97b2c
13241 <13241@ecam.be>
2017-06-20 09:58:36 +0200

    commentary

--- Kitbox/Order.cs
+++ Kitbox/Order.cs
@@ -59,11 +59,16 @@ namespace Kitbox
 
 			return list;
 		}
+
+        /// <summary>
+        /// Returns a dictionnary containing all the information about an order
+        /// </summary>
+        /// <returns>The bill.</returns>
+        /// <param name="order_id">Order identifier.</param>
 		public Dictionary<string, object> GetBill(int order_id)
 		{
             bill = new Dictionary<string, object>();
             Dictionary<string, Dictionary<string, List<object>>> components = new Dictionary<string, Dictionary<string, List<object>>>();
-            //!!!!!!!!!!!!!! We can just make a components.Count to  know how many wardrobes there are
             int wardrobes = components.Count;
             List<List<object>> list = new List<List<object>>();
 
@@ -76,19 +81,27 @@ namespace Kitbox
 			list = DbOrder.DbSearchOrder(OrderTableName, "Header_Bill", string.Format("WHERE (Order_Id ='{0}' AND Client_Id ='{1}')", order_id, current_client.Id));
 			bill.Add("Header", list[0][0]);
 			columnNames = "DISTINCT Wardrobe_Id";
-            // list of the wardrobe Ids
+
+            // List of the wardrobe Id's
 			List<List<object>> list1 = DbOrder.DbSearchOrder(RelTableName, columnNames, condition);
 			bill.Add("Components", new Dictionary<string, Dictionary<string, List<object>>>());
+
+            // Choose a wardrobe
 			for (int i = 1; i <= list1.Count; i++)
 			{
 			    columnNames = "DISTINCT Box_Id";
 				condition = string.Format("WHERE (Order_Id = '{0}' AND Wardrobe_Id = '{1}')", order_id, i);
+
+                // Add the wardrobe as a dictionnary key if it doesn't already exist
                 if(!components.ContainsKey(String.Format("Wardrobe{0}", i)))
                 {
                     components.Add(String.Format("Wardrobe{0}", i), new Dictionary<string, List<object>>());
 				}
-                // list of the box Id depending on Order_Id and Wardrobe_Id
+
+                // Read all the Box_Id's of the database for this wardrobe and this order id
                 List<List<object>>  list2 = DbOrder.DbSearchOrder(RelTableName, columnNames, condition);
+
+				// Choose a box
 				for (int j = 1; j <= list2.Count; j++)
                 {
 					columnNames = "Component_Id";
@@ -96,13 +109,16 @@ namespace Kitbox
 					
 					List<List<object>> list3 = DbOrder.DbSearchOrder(RelTableName, columnNames, condition);
 
+					// Choose a component(part) of the box
 					for (int k = 1; k <= list3.Count; k++)
 					{
-                        //components.Add(String.Format("Wardrobe{0}", i));
-						//condition = Convert.ToString(list[k-1][0]);
 						columnNames = "Ref, Code, Dimensionscm, Couleur, hauteur, profondeur, largeur, PrixClient";
 						condition = string.Format("WHERE Id = '{0}'", Convert.ToString(list3[k-1][0]));
+
+                        // Read all the specifications of a component in the database
                         List<List<object>> list4 = DbOrder.DbSearchOrder(catalog_table_name, columnNames, condition);
+
+                        // Choose the specifications of the component
 						foreach (List<object> component in list4)
 						{
                             
@@ -110,7 +126,6 @@ namespace Kitbox
                             {
                                 components[String.Format("Wardrobe{0}", i)].Add(Convert.ToString(component[1]),new List<object>());
                                 List<object> component_code = components[String.Format("Wardrobe{0}", i)][Convert.ToString(component[1])];
-                                //component_code = new List<object>();
                                 //Quantity
                                 component_code.Add(1);
                                 //Price
@@ -118,20 +133,18 @@ namespace Kitbox
                                 //Reference
                                 component_code.Add(component[0]);
                                 //Dimensions
-                               
                                 component_code.Add(component[2]);
                                 //Colour
                                 component_code.Add(component[3]);
                                 //Height
-                           
                                 component_code.Add(component[4]);
                                 //Depth
-                            
                                 component_code.Add(component[5]);
                                 //Width
-
                                 component_code.Add(component[6]);
                             }
+
+                            // In case the component is already in the dictionnary, we only increment the quantity
                             else 
                             {
                                 List<object> component_code = components[String.Format("Wardrobe{0}", i)][Convert.ToString(component[1])];
