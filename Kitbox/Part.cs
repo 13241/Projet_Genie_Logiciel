50358e4e16c1b0081a31f965c173b99726b3471b
13241 <13241@ecam.be>
2017-06-21 14:27:43 +0200

    commentary part.cs

--- Kitbox/Part.cs
+++ Kitbox/Part.cs
@@ -42,6 +42,12 @@ namespace Kitbox
 
         public abstract void ConstructVisualPart();
 
+        /// <summary>
+        /// this method can be used to modifiy multiple attributes at the same time as long as they exist
+        /// i.e. you have to set the attributes for multiple parts and some of the attributes are the same for every part
+        /// you use this method to set the non-changing attributes, and the setters for the changing-attributes
+        /// </summary>
+        /// <param name="data"></param>
         public void SetData(Dictionary<string, object> data)//MODIF FROM DATABASE
         {
             foreach(string key in data.Keys)
