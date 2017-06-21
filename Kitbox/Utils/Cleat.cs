d4c18df7e89352424873f8e54cf8d19c17d532d4
13241 <13241@ecam.be>
2017-06-21 14:52:18 +0200

    new working repositories

---
+++ Kitbox/Utils/Cleat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Kitbox
+{
+    public class Cleat : Part //tasseau (avec rainures pour glisser les panneaux)
+    {
+        /// <summary>
+        /// the cleat does not need a viusalPart in this application
+        /// </summary>
+        public override void ConstructVisualPart()
+        {
+            //not implemented (this part need not be seen)
+        }
+
+    }
+}
